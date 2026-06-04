1c1bd54a4b3e22cae81428e63a6faa196547fca8
amartyam12bit <my.name.is.apu75@gmail.com>
2026-06-04 16:27:30 +0530

    small fix

--- server.py
+++ server.py
@@ -39,8 +39,4 @@ def get_test():
 
 
 if __name__ == "__main__":
-    mcp.run(
-        transport="streamable-http",
-        host="0.0.0.0",
-        port=8000
-    )
+    mcp.run(transport="streamable-http")
